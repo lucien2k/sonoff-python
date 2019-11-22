9c094860a61c627fc5cb55c7208b1c66326580b0
lucien2k <acowan@gmail.com>
2019-11-22 20:53:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='sonoff-python',
-      version='0.1',
+      version='0.2',
       description='Make use of your sonoff smart switches without flashing them via the cloud APIs',
       long_description=long_description,
       long_description_content_type="text/markdown",
